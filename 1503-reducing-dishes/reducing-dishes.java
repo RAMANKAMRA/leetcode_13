17a47d6a8c804c85d30aada516d4933ed7deee58
Ramanjot Singh <157111424+RAMANKAMRA@users.noreply.github.com>
2025-12-05 02:07:31 +0530

    Time: 5 ms (76.09%) | Memory: 44.1 MB (49.43%) - LeetSync

---
+++ 1503-reducing-dishes/reducing-dishes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
+        int currsum=0;
+        int totalsum=0;
+        for(int i=satisfaction.length-1;i>=0;i--){
+            currsum+=satisfaction[i];
+            if(currsum<0)break;
+            totalsum+=currsum;
+        }
+        return totalsum;
+        
+    }
+}
