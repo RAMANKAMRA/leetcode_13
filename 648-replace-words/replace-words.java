4a0e6fab11158730f8f5490899cc5016ceaa5cc9
Ramanjot Singh <157111424+RAMANKAMRA@users.noreply.github.com>
2026-03-21 00:01:28 +0530

    Time: 758 ms (5.04%) | Memory: 55.1 MB (96.62%) - LeetSync

---
+++ 648-replace-words/replace-words.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        Set<String>st=new HashSet<>(dictionary);
+        String[] words=sentence.split(" ");
+        for(int i=0;i<words.length;i++){
+            String word=words[i];
+            String pre="";
+            for(int j=0;j<word.length();j++){
+                pre+=word.charAt(j);
+                if(st.contains(pre)){
+                    words[i]=pre;
+                    break;
+
+                }
+            }
+        }
+        return String.join(" ",words);
+        
+    }
+}
