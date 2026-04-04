9756394c6719fd9355da9482cafa6f136d4428e8
Ramanjot Singh <157111424+RAMANKAMRA@users.noreply.github.com>
2026-04-04 13:32:27 +0530

    Time: 622 ms (24.96%) | Memory: 43 MB (80.16%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -5,12 +5,12 @@ class Solution {
     }
     public int solve(int[]nums,int i,int target){
         if(i==nums.length){
-      if(target==0)return 1;
-      return 0;
+            if(target==0)return 1;
+            return 0;
         }
-      int add=solve(nums,i+1,target-nums[i]);
-      int sub=solve(nums,i+1,target+nums[i]);
-      return add+sub;
-
+       int add=solve(nums,i+1,target-nums[i]);
+       int sub=solve(nums,i+1,target+nums[i]);
+       return add+sub;
+       
     }
 }
