40b3f0d79c6caf7694e796bc88d6c7695e435331
Ramanjot Singh <157111424+RAMANKAMRA@users.noreply.github.com>
2026-06-30 20:12:50 +0530

    Time: 4 ms (74.78%) | Memory: 45.3 MB (36.31%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int arr[]=new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            arr[i]=-1;
-            for(int j=0;j<nums2.length;j++){
-                if(nums2[j]==nums1[i]){
-                    for(int k=j+1;k<nums2.length;k++){
-                        if(nums2[k]>nums1[i]){
-                            arr[i]=nums2[k];
-                            break;
-                        }
-                    }
-                 
-                }
+        Stack<Integer>st=new Stack<>();
+        HashMap<Integer,Integer>mp=new HashMap<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!st.isEmpty()&&st.peek()<=nums2[i]){
+                st.pop();
+            }if(st.isEmpty()){
+                mp.put(nums2[i],-1);
+            }else{
+                mp.put(nums2[i],st.peek());
             }
-
+            st.push(nums2[i]);
         }
-        return arr;
+        int[]ans=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            ans[i]=mp.get(nums1[i]);
+        }
+        return ans;
         
-
     }
 }
