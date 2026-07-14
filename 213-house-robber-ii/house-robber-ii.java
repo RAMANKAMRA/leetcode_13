ee466dd251187894a7055cf767add6c0806ea785
Ramanjot Singh <157111424+RAMANKAMRA@users.noreply.github.com>
2026-07-15 00:20:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (60.61%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -7,19 +7,18 @@ class Solution {
         int[]dp2=new int[n];
         Arrays.fill(dp1,-1);
         Arrays.fill(dp2,-1);
-        int case1=solve(n-2,nums,0,dp1);
-        int case2=solve(n-1,nums,1,dp2);
-        return Math.max(case1,case2);
+         int case1=solve(n-2,nums,0,dp1);
+         int case2=solve(n-1,nums,1,dp2);
+         return Math.max(case1,case2);
         
     }
+
     public int solve(int i,int[]nums,int start,int[]dp){
         if(i<start)return 0;
-        if(i==start)return nums[i];
-
+        if(i==start)return nums[start];
         if(dp[i]!=-1)return dp[i];
         int pick=nums[i]+solve(i-2,nums,start,dp);
         int notpick=solve(i-1,nums,start,dp);
         return dp[i]=Math.max(pick,notpick);
-
     }
 }
