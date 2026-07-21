a35cd5c3a80ac805284c6f0ec5f8f41ecbcb1fc8
Ramanjot Singh <157111424+RAMANKAMRA@users.noreply.github.com>
2026-07-21 15:17:16 +0530

    Time: 30 ms (74.80%) | Memory: 54.4 MB (45.14%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,15 @@
+class StockSpanner {
+    Stack<int[]> stack = new Stack<>();
+    public StockSpanner() {}
+    public int next(int price) {
+        int result = 1;
+        while(!stack.isEmpty() && stack.peek()[0] <= price) result += stack.pop()[1];
+        stack.push(new int[]{price, result});
+        return result;
+    }
+}
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
