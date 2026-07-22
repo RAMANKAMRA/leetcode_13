c9e7975518957445f3177a2b6b531b57e8ba2ff4
Ramanjot Singh <157111424+RAMANKAMRA@users.noreply.github.com>
2026-07-22 17:41:14 +0530

    Time: 20 ms (12.11%) | Memory: 44.9 MB (89.73%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.java
+++ 437-path-sum-iii/path-sum-iii.java
@@ -1,28 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
     public int pathSum(TreeNode root, int targetSum) {
-
-        if (root == null)
-            return 0;
-
-        return count(root, targetSum)
-                + pathSum(root.left, targetSum)
-                + pathSum(root.right, targetSum);
+        if(root==null)return 0;
+        return count(root,targetSum)+
+        pathSum(root.left,targetSum)+pathSum(root.right,targetSum);
+        
     }
+public int count(TreeNode root,long targetSum){
+    if(root==null)return 0;
+    int ans=0;
+    if(root.val==targetSum)
+        ans++;
 
-    private int count(TreeNode root, long targetSum) {
+        ans+=count(root.left,targetSum-root.val);
+        ans+=count(root.right,targetSum-root.val);
+    return ans;
 
-        if (root == null)
-            return 0;
-
-        int ans = 0;
-
-        if (root.val == targetSum)
-            ans++;
-
-        ans += count(root.left, targetSum - root.val);
-        ans += count(root.right, targetSum - root.val);
-
-        return ans;
-    }
+}
 }
