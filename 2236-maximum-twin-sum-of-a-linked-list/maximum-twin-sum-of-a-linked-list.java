d511a53177dabf849b78b2455d92fee62db49fca
Ramanjot Singh <157111424+RAMANKAMRA@users.noreply.github.com>
2026-07-31 21:13:38 +0530

    Time: 11 ms (20.13%) | Memory: 116 MB (8.42%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -10,21 +10,37 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        ArrayList<Integer>list=new ArrayList<>();
-        while(head!=null){
-            list.add(head.val);
-            head=head.next;
-        }
-        int i=0;
-        int j=list.size()-1;
-        int max=0;
-        while(i<j){
-            max=Math.max(max,list.get(i)+list.get(j));
-            i++;
-            j--;
+        // ArrayList<Integer>list=new ArrayList<>();
+        // while(head!=null){
+        //     list.add(head.val);
+        //     head=head.next;
+        // }
+        // int i=0;
+        // int j=list.size()-1;
+        // int max=0;
+        // while(i<j){
+        //     max=Math.max(max,list.get(i)+list.get(j));
+        //     i++;
+        //     j--;
 
-        }
-        return max;
+        // }
+        // return max;
+
+ArrayList<Integer>list=new ArrayList<>();
+while(head!=null){
+    list.add(head.val);
+    head=head.next;
+}
+int i=0;
+int j=list.size()-1;
+int max=0;
+while(i<j){
+    max=Math.max(max,list.get(i)+list.get(j));
+    i++;
+    j--;
+
+}
+return max;
 
 
         
