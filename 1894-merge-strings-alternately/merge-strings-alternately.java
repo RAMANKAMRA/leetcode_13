69523940565899c06cc763abd579549d1e99f841
Ramanjot Singh <157111424+RAMANKAMRA@users.noreply.github.com>
2026-08-04 19:21:39 +0530

    Time: 1 ms (96.08%) | Memory: 43 MB (78.29%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,6 +1,5 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        
         StringBuilder sb=new StringBuilder();
         int i=0;
         while(i<word1.length()||i<word2.length()){
@@ -9,10 +8,10 @@ class Solution {
             }
             if(i<word2.length()){
                 sb.append(word2.charAt(i));
+                
             }
             i++;
         }
         return sb.toString();
-
     }
 }
